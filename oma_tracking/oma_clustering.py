2e1f9eb6926a95607a19b0e92f8fe8c385638f3f
MAXIMILLIAN WEIL <maximillian.weil@vub.be>
2023-01-31 17:56:31 +0100

    First hdbscan try was a succes

--- oma_tracking/oma_clustering.py
+++ oma_tracking/oma_clustering.py
@@ -4,9 +4,48 @@ oma_clustering.py is a module for clustering modes using DBSCAN algorithm.
 Author: Maximillian Weil
 """
 from dataclasses import dataclass, field
-from sklearn.cluster import DBSCAN
+
+import hdbscan
 import pandas as pd
+from sklearn.cluster import DBSCAN
+
 from oma_tracking.utils import check_columns
+
+
+def data_selection(
+    modal_data: pd.DataFrame,
+    cols: list,
+    min_size: float,
+    max_damping: float
+    ):
+    # feature selection
+    dbscan_data = modal_data[cols]
+    # remove clusters with small size and very high damping as these are non-physical
+    dbscan_data = dbscan_data[dbscan_data['size'] > min_size]
+    dbscan_data = dbscan_data[dbscan_data['damping'] < max_damping]
+
+    return dbscan_data
+
+
+def column_multiplier(
+    modal_data: pd.DataFrame,
+    cols: list,
+    multipliers: dict[str,float],
+    index_divider: float
+    ):
+    multiplied_data = modal_data.copy()
+    # Remove timestamps as index to allow for time gaps in monitoring
+    multiplied_data.reset_index(inplace=True)
+    multiplied_data = multiplied_data[cols]
+    for key in multipliers:
+        if 'damping' in key:
+            multiplied_data[key] = (multiplied_data[key] + 1)
+        multiplied_data[key] = multiplied_data[key] * multipliers[key]
+    # Include the index dimension to the clustering
+    multiplied_data["time_diff"] = (
+        multiplied_data.index.astype(float) - multiplied_data.index.values[0].astype(float)
+    ) / index_divider
+    return multiplied_data
 
 
 @dataclass
@@ -30,19 +69,23 @@ class ModeClusterer:
 
     eps: float = 5
     min_samples: int = 100
-    freq_multiplier: float = 40
-    damping_multiplier: float = 1
-    size_multiplier: float = 0.5
+    multipliers: dict[str,float] = \
+        field(default_factory=lambda: {"frequency": 40, "size": 0.5, "damping": 1})
     index_divider: float = 20000
-    cols: list = field(default_factory=lambda: ["frequency", "size", "damping"])
-    min_modal_size: float = 5
-    max_modal_damping: float = 5
+    cols: list[str] = \
+        field(default_factory=lambda: ["frequency", "size", "damping"])
+    min_size: float = 5.0
+    max_damping: float = 5.0
 
     def __post_init__(self):
         self.dbsc = DBSCAN(eps=self.eps, min_samples=self.min_samples)
         self.dbscan_data: pd.DataFrame = pd.DataFrame()
 
-    def fit(self, modal_data: pd.DataFrame):
+    def fit(
+        self,
+        modal_data: pd.DataFrame,
+        **kwargs
+        ):
         """Fit the modal_data to the DBSCAN algorithm
         for the time period between start_time and end_time.
 
@@ -53,36 +96,25 @@ class ModeClusterer:
             raise ValueError(
                 "The modal data does not contain all the required columns."
             )
-        # feature selection
-        dbscan_data = modal_data[self.cols]
-        # remove clusters with small size and very high damping as these are non-physical
-        dbscan_data = dbscan_data[modal_data["size"] > self.min_modal_size]
-        dbscan_data = dbscan_data[dbscan_data["damping"] < self.max_modal_damping]
-
-        # Remove timestamps as index to allow for time gaps in monitoring
-        indices = dbscan_data.index
-        dbscan_data.reset_index(inplace=True)
-        dbscan_data = dbscan_data[self.cols]
+        dbscan_data = \
+            data_selection(
+                modal_data,
+                self.cols,
+                self.min_size,
+                self.max_damping
+            )
 
         ## feature construction
-        # Multiply the features to increase or decrease the importance
-        dbscan_data["damping"] = (dbscan_data["damping"] + 1) * self.damping_multiplier
-        dbscan_data["frequency"] = dbscan_data["frequency"] * self.freq_multiplier
-        dbscan_data["size"] = dbscan_data["size"] * self.size_multiplier
-        # Include the index dimension to the clustering
-        dbscan_data["time_diff"] = (
-            dbscan_data.index.astype(float) - dbscan_data.index.values[0].astype(float)
-        ) / self.index_divider
+        multiplied_dbscan_data = \
+            column_multiplier(
+                dbscan_data,
+                self.cols,
+                self.multipliers,
+                self.index_divider
+            )
 
-        self.dbsc = DBSCAN(eps=self.eps, min_samples=self.min_samples).fit(dbscan_data)
-
-        # Reset the features to their original values
-        dbscan_data["frequency"] = dbscan_data["frequency"] / self.freq_multiplier
-        dbscan_data["damping"] = dbscan_data["damping"] / self.damping_multiplier - 1
-        dbscan_data["size"] = dbscan_data["size"] / self.size_multiplier
-        dbscan_data.set_index(indices, inplace=True)
+        self.dbsc = DBSCAN(eps=self.eps, min_samples=self.min_samples).fit(multiplied_dbscan_data)
         dbscan_data["labels"] = self.dbsc.labels_
-
         self.dbscan_data = dbscan_data
 
     def predict(self, min_cluster_size: int = 500) -> pd.DataFrame:
@@ -107,6 +139,104 @@ class ModeClusterer:
 
         clustered_data = self.dbscan_data[self.dbscan_data["labels"].isin(lbls)][
             self.dbscan_data[self.dbscan_data["labels"].isin(lbls)]["labels"] >= 0
+        ]
+        # Reset the modes to start from 0
+        codes, uniques = pd.factorize(clustered_data["labels"])
+        clustered_data["labels"] = codes
+        return clustered_data
+
+
+
+@dataclass
+class ModeClusterer_HDBSCAN:
+    """ModeClusterer is a class for clustering mode parameters using HDBSCAN algorithm.
+
+    Attributes:
+        freq_multiplier (float): The multiplier for the frequency feature.
+        damping_multiplier (float): The multiplier for the damping feature.
+        size_multiplier (float): The multiplier for the size feature.
+        cols (list): The columns of the dataframe that should be used for clustering.
+        min_modal_size (float): The minimum size of a mode to be considered for clustering.
+        max_modal_damping (float): The maximum damping of a mode to be considered for clustering.
+        dbsc (DBSCAN): The DBSCAN object that stores the result of the clustering.
+        dbscan_data (pd.DataFrame): The dataframe that is used for clustering.
+    """
+
+    min_cluster_size: int = 100
+    multipliers: dict[str,float] = \
+        field(default_factory=lambda: {"frequency": 40, "size": 0.5, "damping": 1})
+    index_divider: float = 20000
+    cols: list[str] = \
+        field(default_factory=lambda: ["frequency", "size", "damping"])
+    min_size: float = 5.0
+    max_damping: float = 5.0
+
+    def __post_init__(self):
+        self.dbsc = hdbscan.HDBSCAN(min_cluster_size=self.min_cluster_size)
+        self.hdbscan_data: pd.DataFrame = pd.DataFrame()
+
+    def fit(
+        self,
+        modal_data: pd.DataFrame,
+        **kwargs
+        ):
+        """Fit the modal_data to the HDBSCAN algorithm
+        for the time period between start_time and end_time.
+
+        Args:
+            modal_data (pd.DataFrame): The modal data to be fitted.
+        """
+        if not check_columns(self.cols, modal_data):
+            raise ValueError(
+                "The modal data does not contain all the required columns."
+            )
+        hdbscan_data = \
+            data_selection(
+                modal_data,
+                self.cols,
+                self.min_size,
+                self.max_damping
+            )
+
+        ## feature construction
+        multiplied_dbscan_data = \
+            column_multiplier(
+                hdbscan_data,
+                self.cols,
+                self.multipliers,
+                self.index_divider
+            )
+
+        self.dbsc = \
+            hdbscan.HDBSCAN(
+                min_cluster_size=self.min_cluster_size,
+                **kwargs
+            ).fit(multiplied_dbscan_data)
+        hdbscan_data["labels"] = self.dbsc.labels_
+        self.hdbscan_data = hdbscan_data
+
+    def predict(self, min_cluster_size: int = 500) -> pd.DataFrame:
+        """Predict the clusters of the fitted data
+        that have more clusters than the min_cluster_size.
+
+        Args:
+            min_cluster_size (int, optional): The minimum number of modes in a cluster.
+                Defaults to 500.
+
+        Returns:
+            pd.DataFrame: The dataframe with the predicted clusters.
+        """
+
+        clustered_data = self.hdbscan_data.copy()
+        # Remove clusters with less than min_cluster_size samples
+        lbls = []
+        for label in self.hdbscan_data["labels"].unique():
+            cnt = len(self.hdbscan_data[self.hdbscan_data["labels"] == label])
+            if cnt > min_cluster_size:
+                lbls.append(label)
+
+        clustered_data = self.hdbscan_data[self.hdbscan_data["labels"].isin(lbls)][
+            self.hdbscan_data[self.hdbscan_data["labels"].isin(lbls)]["labels"] >= 0
         ]
         # Reset the modes to start from 0
         codes, uniques = pd.factorize(clustered_data["labels"])
